e36ca64ed375a648e800c98ad789e819545ad688
RAUSHAN KUMAR <160950026+Raushan2709@users.noreply.github.com>
2024-04-18 18:49:14 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the number\n");
+    scanf("%d%d",&a,&b);
+    int add = a+b;
+    printf("addition is = %d\n",add);
+    return 0;
+
+}
